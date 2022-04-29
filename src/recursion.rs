b123c7e63d27eaf818ba67be0a362ef7efc53fe2
Mika Yoshida <yoshidam@kuronekoyamato.co.jp>
2022-04-29 16:26:06 +0900

    implement new functions p102

--- src/recursion.rs
+++ src/recursion.rs
@@ -1,5 +1,5 @@
 #[warn(dead_code)]
-fn add_to_each(n: i32, mut lst: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+fn add_to_each(n: i32, lst: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let res = match lst.len() {
         0 => Vec::<Vec<i32>>::new(),
         _ => {
@@ -15,7 +15,7 @@ fn add_to_each(n: i32, mut lst: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 }
 
 #[warn(dead_code)]
-fn prefix(mut lst: Vec<i32>) -> Vec<Vec<i32>> {
+fn prefix(lst: Vec<i32>) -> Vec<Vec<i32>> {
     let res = match lst.len() {
         0 => Vec::<Vec<i32>>::new(),
         _ => {
@@ -57,8 +57,41 @@ fn ins_sort(lst: Vec<i32>) -> Vec<i32> {
     answer
 }
 
+#[warn(dead_code)]
+fn minimum(lst: Vec<i32>) -> i32 {
+    let res = match lst.len() {
+        0 => i32::MAX,
+        _ => {
+            let m = minimum(lst[1..].to_vec());
+            if lst[0] <= m {
+                lst[0]
+            } else {
+                m
+            }
+        }
+    };
+    res
+}
+
+#[warn(dead_code)]
+fn append<T: Clone+Copy>(lst1: Vec<T>, lst2: Vec<T>) -> Vec<T> {
+    let res = match lst1.len() {
+        0 => lst2,
+        _ => {
+            let first = lst1[0];
+            let rest = lst1[1..].to_vec();
+            let mut ret = vec![first];
+            ret.append(&mut append(rest, lst2));
+            ret
+        }
+    };
+    res
+}
+
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
     #[test]
     fn add_to_each_1() {
@@ -107,5 +140,37 @@ mod tests {
     #[test]
     fn ins_sort_4() {
         assert_eq!(ins_sort(vec![-1, -4, 4, 1]), vec![-4, -1, 1, 4])
+    }
+    #[test]
+    fn minimum_1() {
+        assert_eq!(minimum(Vec::<i32>::new()), i32::MAX)
+    }
+    #[test]
+    fn minimum_2() {
+        assert_eq!(minimum(vec![1, 3, 5]), 1)
+    }
+    #[test]
+    fn minimum_3() {
+        assert_eq!(minimum(vec![3, 2]), 2)
+    }
+    #[test]
+    fn append_1(){
+        assert_eq!(append(Vec::<i32>::new(), Vec::<i32>::new()), vec![])
+    }
+    #[test]
+    fn append_2() {
+        assert_eq!(append(vec![], vec![1, 2]), vec![1, 2])
+    }
+    #[test]
+    fn append_3() {
+        assert_eq!(append(vec![1, 2], vec![]), vec![1, 2])
+    }
+    #[test]
+    fn append_4() {
+        assert_eq!(append(vec![1, 2], vec![3, 4]), vec![1, 2, 3, 4])
+    }
+    #[test]
+    fn append_5() {
+        assert_eq!(append(vec!['a'], vec!['b']), vec!['a', 'b'])
     }
 }
