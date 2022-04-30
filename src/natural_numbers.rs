6dae65b537f328347102bf190349bad6633bb5d3
Mika Yoshida <yoshidam@kuronekoyamato.co.jp>
2022-04-30 14:52:55 +0900

    add power function and tests

--- src/natural_numbers.rs
+++ src/natural_numbers.rs
@@ -7,6 +7,15 @@ fn factorial(n: i32) -> i32 {
     res
 }
 
+fn power(i: i32, j: i32) -> i32 {
+    let res = match j {
+        0 => 1,
+        1 => i,
+        _ => i * power(i, j-1),
+    };
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,5 +34,17 @@ mod tests {
     #[test]
     fn test_factorial_4() {
         assert_eq!(factorial(10), 3628800)
+    }
+    #[test]
+    fn test_power_1() {
+        assert_eq!(power(3, 0), 1)
+    }
+    #[test]
+    fn test_power_2() {
+        assert_eq!(power(3, 1), 3)
+    }
+    #[test]
+    fn test_power_3() {
+        assert_eq!(power(3, 2), 9)
     }
 }
